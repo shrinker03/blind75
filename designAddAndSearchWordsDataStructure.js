c9c69ee3391c3a9f83a708abce1e6eee995db07e
DESKTOP-65CV4VO\Acer <shivamdamre913@gmail.com>
2025-04-17 21:51:42 +0530

    Added readme in design and add search word ds

---
+++ designAddAndSearchWordsDataStructure.js
@@ -0,0 +1,59 @@
+/*
+
+This needs to be solved again
+
+
+*/
+
+var WordDictionary = function () {
+    this.root = {};
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function (word) {
+    let node = this.root;
+
+    for (let char of word) {
+        if (!node[char]) {
+            node[char] = {};
+        }
+        node = node[char]
+    }
+    node.isEnd = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function (word) {
+
+    function dfs(j, root) {
+        let node = root;
+        for (let i = j; i < word.length; i++) {
+            if (word[i] === '.') {
+                for (let child of Object.values(node)) {
+                    if (dfs(i + 1, child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (!node[word[i]]) return false;
+                node = node[word[i]]
+            }
+        }
+        return !!node.isEnd;
+    }
+    return dfs(0, this.root)
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
