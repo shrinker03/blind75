831930f4c4410c14fb03efb3474329a6b79e89dd
DESKTOP-65CV4VO\Acer <shivamdamre913@gmail.com>
2025-05-07 21:42:15 +0530

    Palindromic Substring

---
+++ palindromicSubString.js
@@ -0,0 +1,51 @@
+/* Level => 4/5 => If done previous, this can be skipped
+
+🧠 Problem: Count Substrings (Palindromic)
+Leetcode: 647. Palindromic Substrings
+Category: Expand Around Center, String
+
+🔍 Problem Summary:
+Given a string s, return the number of palindromic substrings in it. A substring is palindromic if it reads the same forward and backward.
+
+💡 Approach:
+
+For each index i in the string, expand around:
+
+Single center (odd-length palindromes)
+
+Double center (even-length palindromes)
+
+Count and accumulate valid expansions
+
+🧩 Time Complexity: O(n²)
+📦 Space Complexity: O(1)
+
+
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function (s) {
+    let res = 0;
+
+    function countPalindrom(left, right, s) {
+        let res = 0;
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            res += 1;
+            left -= 1;
+            right += 1;
+        }
+        return res;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        res += countPalindrom(i, i, s); //odd
+        res += countPalindrom(i, i + 1, s); //even
+    }
+
+    return res;
+
+};
